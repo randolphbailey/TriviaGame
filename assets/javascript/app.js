3eb83d52ae00d719f77e40394c3adc79084271e2
Randolph Bailey <Randolph Bailey>
2019-01-14 13:55:24 -0600

    more code cleanup and consolidation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 //set questions, answers, and other values
-var questions = {
-    q1: {
+var questions = [
+    {
         worth: 100,
         qText: "In the 'Road Runner and Coyote' cartoons, what famous sound does the Road Runner make?",
         a: "Ping! Ping!",
@@ -10,7 +10,7 @@ var questions = {
         correctAns: "Beep! Beep!",
         correctDiv: "bdiv"
     },
-    q2: {
+    {
         worth: 200,
         qText: "Where should choking victims place their hands to indicate to others that they need help?",
         a: "Over the eyes",
@@ -20,7 +20,7 @@ var questions = {
         correctAns: "Around the throat",
         correctDiv: "cdiv"
     },
-    q3: {
+    {
         worth: 300,
         qText: "Which of these dance names is used to describe a fashionable dot?",
         a: "Hora",
@@ -30,7 +30,7 @@ var questions = {
         correctAns: "Polka",
         correctDiv: "ddiv"
     },
-    q4: {
+    {
         worth: 500,
         qText: "In what 'language' would you say 'ello-hay' to greet your friends?",
         a: "Bull Latin",
@@ -40,7 +40,7 @@ var questions = {
         correctAns: "Pig Latin",
         correctDiv: "ddiv"
     },
-    q5: {
+    {
         worth: 1000,
         qText: "What part of a chicken is commonly called the 'drumstick'?",
         a: "Breast",
@@ -50,7 +50,7 @@ var questions = {
         correctAns: "Leg",
         correctDiv: "cdiv"
     },
-    q6: {
+    {
         worth: 2000,
         qText: "What is the only position on a football team that can be 'sacked'?",
         a: "Center",
@@ -60,7 +60,7 @@ var questions = {
         correctAns: "Quarterback",
         correctDiv: "ddiv"
     },
-    q7: {
+    {
         worth: 4000,
         qText: "What god of love is often depicted as a chubby winged infant with a bow and arrow?",
         a: "Zeus",
@@ -70,7 +70,7 @@ var questions = {
         correctAns: "Cupid",
         correctDiv: "cdiv"
     },
-    q8: {
+    {
         worth: 8000,
         qText: "What Steven Spielberg film climaxes at a place called Devil's Tower?",
         a: "E.T.: The Extra-Terrestrial",
@@ -80,7 +80,7 @@ var questions = {
         correctAns: "Close Encounters of the Third Kind",
         correctDiv: "ddiv"
     },
-    q9: {
+    {
         worth: 16000,
         qText: "In what U.S. town did the famous 1881 shoot-out at the O.K. Corral take place?",
         a: "Laramie",
@@ -90,7 +90,7 @@ var questions = {
         correctAns: "Tombstone",
         correctDiv: "bdiv"
     },
-    q10: {
+    {
         worth: 32000,
         qText: "Which of the following months has no U.S. federal holiday?",
         a: "August",
@@ -100,7 +100,7 @@ var questions = {
         correctAns: "August",
         correctDiv: "adiv"
     },
-    q11: {
+    {
         worth: 64000,
         qText: "What mythological beast is reborn from its own ashes?",
         a: "Phoenix",
@@ -110,7 +110,7 @@ var questions = {
         correctAns: "Phoenix",
         correctDiv: "adiv"
     },
-    q12: {
+    {
         worth: 125000,
         qText: "Who developed the first effective vaccine against polio?",
         a: "Albert Sabin",
@@ -120,7 +120,7 @@ var questions = {
         correctAns: "Jonas Salk",
         correctDiv: "ddiv"
     },
-    q13: {
+    {
         worth: 250000,
         qText: "Which of the following is not a monotheistic religion?",
         a: "Islam",
@@ -130,7 +130,7 @@ var questions = {
         correctAns: "Hinduism",
         correctDiv: "cdiv"
     },
-    q14: {
+    {
         worth: 500000,
         qText: "What architect designed the glass pyramid in the courtyard of the Louvre?",
         a: "Phillip Johnson",
@@ -140,7 +140,7 @@ var questions = {
         correctAns: "I.M. Pei",
         correctDiv: "ddiv"
     },
-    q15: {
+    {
         worth: 1000000,
         qText: "Which of these U.S. Presidents appeared on the television series 'Laugh-In'?",
         a: "Lyndon Johnson",
@@ -149,13 +149,10 @@ var questions = {
         d: "Gerald Ford",
         correctAns: "Richard Nixon",
         correctDiv: "bdiv"
-    }
-}
+    }];
 
-var questionNumber = 1;
-//This might have been simpler with an array but whatever
-var currentQuestion = "q" + questionNumber;
 
+var questionNumber = 0;
 var time;
 var intervalID;
 
@@ -192,30 +189,28 @@ function walkAway() {
     //changes question back to previously correct answer, outputs amount won and updates value list
     $("li").removeClass("activeList");
     questionNumber--;
-    currentQuestion = "q" + questionNumber;
-    $("#" + questions[currentQuestion].worth).addClass("activeList");
-    let moneyText = "Congratulations!  You won " + $("#" + questions[currentQuestion].worth).text() + "!";
+    $("#" + questions[questionNumber].worth).addClass("activeList");
+    let moneyText = "Congratulations!  You won " + $("#" + questions[questionNumber].worth).text() + "!";
     $("#timerText").text(moneyText);
 }
 
 //Loads the next question
 function nextQuestion() {
     //set next question
-    $("#question").text(questions[currentQuestion].qText);
+    $("#question").text(questions[questionNumber].qText);
 
     //set answers for next question
-    $("#aText").text(questions[currentQuestion].a);
-    $("#bText").text(questions[currentQuestion].b);
-    $("#cText").text(questions[currentQuestion].c);
-    $("#dText").text(questions[currentQuestion].d);
+    $("#aText").text(questions[questionNumber].a);
+    $("#bText").text(questions[questionNumber].b);
+    $("#cText").text(questions[questionNumber].c);
+    $("#dText").text(questions[questionNumber].d);
 
     //blank out background colors for active guess and correct answer
-    $("div[target='answer']").removeClass("bg-success text-dark");
-    $("span[target='answerText'").removeClass("selected");
+    $("div[target='answer'], span[target='answerText']").removeClass("bg-success text-dark selected");
 
     //increment question value
     $("li").removeClass("activeList");
-    $("#" + questions[currentQuestion].worth).addClass("activeList");
+    $("#" + questions[questionNumber].worth).addClass("activeList");
 }
 
 //Checks to see if answer is correct or wrong
@@ -224,24 +219,22 @@ function checkAnswers() {
     console.log($(".selected").text());
 
     //check if answer given is correct
-    if ($(".selected").text() == questions[currentQuestion].correctAns) {
+    if ($(".selected").text() == questions[questionNumber].correctAns) {
         
         //stop the timer
         stop();
 
         //increment question number in preparation for loading of next question since the answer was correct
         questionNumber++;
-        currentQuestion = "q" + questionNumber;
 
         //Check to see if contestant has won the million dollars
-        if (currentQuestion == "q16") {
+        if (questionNumber == 15) {
             stop();
             millionaire();
         }
         else {
         //change correct answer background from selected orange to correct green
-        $(".selected").parent().parent().removeClass("bg-warning");
-        $(".selected").parent().parent().addClass("bg-success");
+        $(".selected").parent().parent().removeClass("bg-warning").addClass("bg-success");
 
         //Set timer area to show correct answer
         $("#timerText").text("Correct!");
@@ -254,7 +247,7 @@ function checkAnswers() {
     //run this on wrong answer
     else {
         //Set background of correct answer to green.  Leave selected wrong answer as orange
-        $("#" + questions[currentQuestion].correctDiv).addClass("bg-success");
+        $("#" + questions[questionNumber].correctDiv).addClass("bg-success");
         
         //Stop timer
         stop();
@@ -268,20 +261,23 @@ function checkAnswers() {
 
 //handles click events on answers
 function clicky() {
+    
     //get letter clicked
     let letter = event.target.id;
 
-    //These two lines blank out all answers, ensuring only one can be selected at a time.
-    $("div[target='answer']").removeClass("bg-warning text-dark");
-    $("span[target='answerText'").removeClass("selected");
+    //Log what was clicked for debugging purposes
+    console.log(letter + " click");
+
+    //Clean up retrieved ids
+    letter = letter.slice(0,1);
+
+    //This line blanks out all answers, ensuring only one can be selected at a time.
+    $("div[target='answer'], span[target='answerText']").removeClass("bg-warning text-dark selected");
 
     //Add background color to currently selected answer
     //Currently selected answer is denoted internally by adding a blank "selected" CSS class
     $("#" + letter + "div").addClass("bg-warning text-dark");
     $("#" + letter + "Text").addClass("selected");
-
-    //Log the click for debugging purposes
-    console.log(letter + " click");
 }
 
 
